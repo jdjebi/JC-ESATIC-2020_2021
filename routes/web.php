1ce9e75499c0868ca452508844f4d5caf6753cf9
jdjebi <jeanmarcdjebi@gmail.com>
2020-12-16 23:00:26 +0000

    Mise à jour

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,55 @@ Route::middleware("auth")->group(function(){
   
 });
 
+/* Administration */
+
+Route::prefix('/v1/admin')->group(function (){
+
+    Route::get('connexion','UI\admin\Auth\AuthController@login')->name('admin')->middleware('admin.guest');
+  
+    Route::middleware('admin.login')->group(function (){
+  
+      Route::namespace("UI\admin")->group(function (){
+        Route::get('','AdminController@index')->name('admin_index');
+        Route::get('users','User\UserController@index')->name('admin.users.index');
+        Route::get('user/action/','AdminController@delete_user')->name('admin_delete_user');
+        Route::get('users/{id}','User\UserController@show')->where('id', '[0-9]+')->name('admin_user_profil');
+        Route::get('users/{id}/compte','User\UserController@account')->where('id', '[0-9]+')->name('admin.users.account');
+        Route::get('roles-permissions/','User\RolesAndPermissionsController@index')->name('admin.user.roles');
+        Route::get('roles-permissions/roles/{id}/','User\RolesAndPermissionsController@show')->where('id', '[0-9]+')->name('admin.roles.show');
+      });
+  
+      Route::get('deconnexion','Backend\Auth\AuthController@admin_logout')->name('admin.logout');
+  
+      Route::namespace("Resac")->group(function (){
+        Route::get('rechercher',"SearchController@admin")->name('admin_search');
+      });
+  
+    });
+  
+  });
+  
+  Route::name('admin.')->group(function () {
+  
+    // Frontend
+    Route::namespace('UI\admin')->group(function () {
+    
+      Route::middleware('admin.login')->group(function (){
+        Route::prefix('/v1/admin/')->group(function () {
+  
+          /* Index de recherche utilisateur */
+          Route::get('webengine/index','WebEngineIndexController@show')->name('webengine.show');
+          Route::get('webengine/index/generate','WebEngineIndexController@generate_index')->name('webengine.generate_index');
+          Route::get('webengine/index/clear','WebEngineIndexController@clear_index')->name('webengine.clear_index');
+        });
+  
+        Route::prefix('/v1/admin/')->group(function () {
+          Route::get('dev/flash/creator','DevController@create_flash')->name('dev.create_flash');
+        });
+  
+      });
+  
+    });
+  
+  });
+
