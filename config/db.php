03efc93dbddf0f847270249226e0eb0ae59e5768
jdjebi <jeanmarcdjebi@gmail.com>
2020-12-16 22:41:30 +0000

    Mise à jour

--- config/db.php
+++ config/db.php
@@ -16,7 +16,7 @@ if(!isset($_SERVER["HEROKU_APP_DIR"])){
   $DEBUG = true;
 
   return [
-    "host" => "q7cxv1zwcdlw7699.chr7pe7iynqr.eu-west-1.rds.amazonaws.com	",
+    "host" => "q7cxv1zwcdlw7699.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
     "username" => "sys2gnjehblaiwru",
     "password" => "dcmo6dyci37oxap7",
     "dbname" => "o6jxe6s44ipgpqjp"
