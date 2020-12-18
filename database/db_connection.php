6ac2812227e4a23086070dc5cea4e17a2d092adc
jdjebi <jeanmarcdjebi@gmail.com>
2020-12-18 08:57:02 +0000

    Mise à jour

--- database/db_connection.php
+++ database/db_connection.php
@@ -7,6 +7,7 @@ $username = $config["username"];
 $password = $config["password"];
 $dbname = $config["dbname"];
 
+
 try {
   $db = new PDO("mysql:host=$host;dbname=$dbname",$username,$password);
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
